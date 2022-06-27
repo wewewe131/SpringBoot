8ea4bb3dccae373e41c07198707bbf055cc630fb
wewewe131 <2283204413@qq.com>
2022-06-27 17:03:58 +0800

    编写实体类

---
+++ src/main/java/com/example/springbootdemo/Bean/News.java
@@ -0,0 +1,19 @@
+package com.example.springbootdemo.Bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class News {
+  private String id;
+  private String title;
+  private String content;
+  private Date createdate;
+
+}
